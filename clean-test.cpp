7c519710875e14b0886edea17078f35ac3b7df8f
gireeshhiremath-lab <68851247+gireeshhiremath-lab@users.noreply.github.com>
2020-08-03 16:42:12 +0530

    Update clean-test.cpp

--- clean-test.cpp
+++ clean-test.cpp
@@ -1,5 +1,10 @@
+#include <vehicle-collector.hpp>
 #include <gtest/gtest.h>
- 
+ TEST(COLLECT,when_vehicle_report_a_measurement_it_is_featched_and_stored_with_vehicle_id)
+ {
+  inventory i = telematic_to_inventory(telematics);
+  ASSERT_EQ(i,30);
+ }
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
