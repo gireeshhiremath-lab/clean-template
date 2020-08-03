32872ab8963b4548565d218f0a2bf33dc1f4712c
gireeshhiremath-lab <68851247+gireeshhiremath-lab@users.noreply.github.com>
2020-08-04 00:04:55 +0530

    Update clean-test.cpp

--- clean-test.cpp
+++ clean-test.cpp
@@ -1,4 +1,4 @@
-#include <vehicle-collector.hpp>
+#include "vehicle-collector.hpp"
 #include <gtest/gtest.h>
  TEST(COLLECT, when_vehicle_report_a_measurement_it_is_featched_and_stored_with_vehicle_id)
  {
