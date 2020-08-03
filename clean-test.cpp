a74d6c7357c3cbc93c38f87308ddfe00ff7f3667
gireeshhiremath-lab <68851247+gireeshhiremath-lab@users.noreply.github.com>
2020-08-04 00:08:04 +0530

    Update clean-test.cpp

--- clean-test.cpp
+++ clean-test.cpp
@@ -3,7 +3,7 @@
  TEST(COLLECT, when_vehicle_report_a_measurement_it_is_featched_and_stored_with_vehicle_id)
  {
   telematics vehi_tele = {121, motor_temp, 25};
-  inventory vehi_invt = telematic_to_inventory(telematics);
+  inventory vehi_invt = telematic_to_inventory(vehi_tele);
   ASSERT_EQ(vehi_invt.motor_temp, 25);
  }
  
